726bd9a125cdc725273fd83c34b1809d4cda927b
MJSchreiber <mschreiber@watlow.com>
2017-03-23 13:01:07 -0500

    added swing frame

--- mschreiber2740Ex3h/src/mschreiber2740Ex3h/Rainfall.java
+++ mschreiber2740Ex3h/src/mschreiber2740Ex3h/Rainfall.java
@@ -2,54 +2,64 @@ package mschreiber2740Ex3h;
 
 public class Rainfall {
 	
-	private double [] rainfall;
+	private double [] rainfallArray;
 	
 	
 	
 			
 	//constructor for double array
 	public Rainfall(double [] dr) {
-		this.rainfall = new double [dr.length];
+		this.rainfallArray = new double [dr.length];
 		for (int i = 0; i < dr.length; i++)
-			this.rainfall[i] = dr [i];
+			this.rainfallArray[i] = dr [i];
 	}
 
 	//constructor for string array
-	public Rainfall(String [] strDr) {
-		this.rainfall = new double [strDr.length];
-		for (int i = 0; i < strDr.length; i++)
-			this.rainfall[i] = Double.parseDouble(strDr[i]);
+	public Rainfall(String [] strRa) {
+		this.rainfallArray = new double [strRa.length];
+		for (int i = 0; i < strRa.length; i++)
+			this.rainfallArray[i] = Double.parseDouble(strRa[i]);
 	}
 	
 	//calculates the total of all array items
 	public double getTotal()
 	{
 		double total = 0.0;
-		for (double value : rainfall)
+		for (double value : rainfallArray)
 			total += value;
 		return total;
 	}
 	
 	//calculates the average of the array items
 	public double getAverage()
-	{
-		
-		return getTotal() / rainfall.length;
+	{		
+		return getTotal() / rainfallArray.length;
 	}
 	
 	//get the highest value in the array
 	//FINISH METHODS STARTING 
 	public double getHighest()
 	{
-		double highest = 0.0;
+		double highest = rainfallArray[0];
+		for(int index = 1; index < rainfallArray.length; index++)
+		{
+			if (rainfallArray[index] > highest)
+				highest = rainfallArray[index];
 		
+		}
 		return highest;
 	}
 	
 	//get the lowest value in the array
 	public double getLowest()
 	{
-		double lowest = 0.0;
+		double lowest = rainfallArray[0];
+		for(int index = 1; index < rainfallArray.length; index++)
+		{
+			if (rainfallArray[index] < lowest)
+				lowest = rainfallArray[index];
+		
+		}
 		return lowest;
 	}
 
---
+++ mschreiber2740Ex3h/src/mschreiber2740Ex3h/RainfallForm.java
@@ -0,0 +1,70 @@
+package mschreiber2740Ex3h;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.UIManager;
+
+public class RainfallForm extends JFrame {
+
+	private JPanel contentPane;
+	private JList rainfallList;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RainfallForm frame = new RainfallForm();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RainfallForm() {
+		setTitle("MSchreiber 2740 Ex3H Rainfall");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 562);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMonthlyRainfall = new JLabel("Monthly Rainfall");
+		lblMonthlyRainfall.setBounds(10, 11, 105, 14);
+		contentPane.add(lblMonthlyRainfall);
+		
+		JList monthList = new JList();
+		monthList.setBackground(UIManager.getColor("Label.background"));
+		monthList.setEnabled(false);
+		monthList.setModel(new AbstractListModel() {
+			String[] values = new String[] {"01 Jan", "02 Feb", "03 Mar", "04 Apr", "05 May", "06 June", "07 Jul", "08 Aug", "09 Sept", "10 Oct", "11 Nov", "12 Dec"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		monthList.setBounds(20, 39, 72, 200);
+		contentPane.add(monthList);
+		
+		rainfallList = new JList();
+		rainfallList.setBounds(77, 39, 72, 200);
+		contentPane.add(rainfallList);
+	}
+}
